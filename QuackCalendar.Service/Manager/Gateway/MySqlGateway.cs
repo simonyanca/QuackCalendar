50e3a916ee1009b055e88817963f7cb8fec06afa
Jeff <jefft22@gmail.com>
2019-11-27 16:34:55 -0800

    started implementing getevents() for mysqlgateway

--- QuackCalendar.Service/Manager/Gateway/MySqlGateway.cs
+++ QuackCalendar.Service/Manager/Gateway/MySqlGateway.cs
@@ -1,4 +1,5 @@
-﻿using QuackCalendar.Model;
+﻿using MySql.Data.MySqlClient;
+using QuackCalendar.Model;
 
 namespace QuackCalendar.Service.Manager.Gateway
 {
@@ -6,7 +7,49 @@ namespace QuackCalendar.Service.Manager.Gateway
     {
         protected override QCGetEventsResponse GetEventsCore(QCGetEventsRequest qcGetEventsRequest)
         {
-            return new QCGetEventsResponse();
+            //var connectionString = "server=192.168.1.103;port=3306;user=admin;password=sqlsql";
+            //var connection = new MySqlConnection(connectionString);
+            //connection.Open();
+
+            //var sqlQueryString = "SELECT * FROM quackcalendar.events;";
+            //var command = new MySqlCommand(sqlQueryString, connection);
+            //var returnValue = command.ExecuteReader();
+            //while (returnValue.Read())
+            //{
+            //    string a = string.Empty;
+
+            //    for (int i = 0; i < returnValue.FieldCount; i++)
+            //    {
+            //        a += returnValue[i] + " ";
+            //    }
+
+            //    a = a;
+            //}
+
+            //connection.Close();
+
+            var results = ExecuteQuery("SELECT * FROM quackcalendar.events;", 5);
+            var response = new QCGetEventsResponse();
+
+            return response;
+        }
+
+        private MySqlDataReader ExecuteQuery(string queryString, int expectedColumns)
+        {
+            using (var connection = new MySqlConnection("server=192.168.1.103;port=3306;user=admin;password=sqlsql"))
+            {
+                connection.Open();
+                var command = new MySqlCommand(queryString, connection);
+                var reader = command.ExecuteReader();
+                connection.Close();
+
+                if (reader.FieldCount != expectedColumns)
+                {
+                    throw new System.Exception($"wrong number of expected columns:\nexpected{expectedColumns} but got {reader.FieldCount}");
+                }
+
+                return reader;
+            }
         }
     }
 }
